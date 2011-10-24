7952b0b0f11cfd72cd01bf11fcb88dfad43572b5
James Snyder <jbsnyder@fanplastic.org>
2011-10-23 23:17:33 -0500

    fix for SysTick, system timer is likely working now

--- src/platform/stm32f4/stm32f4xx_it.c
+++ src/platform/stm32f4/stm32f4xx_it.c
@@ -191,15 +191,7 @@ void PendSV_Handler(void)
 {
 }
 
-/**
-  * @brief  This function handles SysTick Handler.
-  * @param  None
-  * @retval None
-  */
-void SysTick_Handler(void)
-{
-  TimingDelay_Decrement();
-}
+
 
 /******************************************************************************/
 /*                 STM32F4xx Peripherals Interrupt Handlers                   */
