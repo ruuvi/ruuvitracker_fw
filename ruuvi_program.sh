2796e88aa926bc2825ec32734c09a92441af9d0a
SeppoTakalo <seppo.takalo@iki.fi>
2012-12-29 09:06:34 +0200

    Added parameter to Reboot device after flash

--- ruuvi_program.sh
+++ ruuvi_program.sh
@@ -1,2 +1,4 @@
+./ruuvi_build.sh
 #st-flash write elua_lua_stm32f407vg.bin 0x08000000
-dfu-util -i 0 -a 0 -D elua_lua_stm32f415rg.bin -s 0x08000000
+dfu-util -i 0 -a 0 -D elua_lua_stm32f415rg.bin -s 0x08000000:leave
+
