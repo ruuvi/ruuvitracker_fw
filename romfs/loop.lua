4fa646e223d99ff5e8a0f49471f22ec385e45aa9
SeppoTakalo <seppo.takalo@iki.fi>
2012-12-29 09:14:47 +0200

    Serial port monitoring helper

---
+++ romfs/loop.lua
@@ -0,0 +1,11 @@
+
+function uart_loop(id)
+	print("Now in loop, send commands\n-----------------")
+	while 1 == 1 do
+	l = uart.read(id,'*l',uart.NO_TIMEOUT)
+	if l ~= "" then uart.write(0,l) end
+	l = uart.read(0,'*l',uart.NO_TIMEOUT)
+	if l ~= "" then uart.write(id,l) end
+	end
+end
+
