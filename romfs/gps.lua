ed02f79cacde540f6167fe6243b965eca2b0fe07
SeppoTakalo <seppo.takalo@iki.fi>
2012-12-30 16:09:58 +0200

    Initial template for GPS driver

--- romfs/gps.lua
+++ romfs/gps.lua
@@ -1,7 +1,30 @@
 require('gsm')
 
-function enable_gps()
-	uart.setup( 1, 115200, 8, uart.PAR_NONE, uart.STOP_1 )
+gps_enabled = 0
+gps_uart = 1
+
+function gps_enable()
+	uart.setup( gps_uart, 115200, 8, uart.PAR_NONE, uart.STOP_1 )
 	gsm_cmd('AT+CGPSPWR=1')
 	gsm_cmd('AT+CGPSRST=0')
+	gps_enabled = 1
+end
+
+function gps_monitor()
+	if gps_enabled==0 then gps_enable() end
+	
+	
+	while 1 == 1 do
+		str = uart.read(gps_uart,'*l')
+		if str:find("^%$GPRMC") then
+				local time, status, latitude, ns_indicator, longitude,
+			  	ew_indicator, speed = str:match("^%$GPRMC,([^,]*),([^,]*),([^,]*),([^,]*),([^,]*),([^,]*),([^,]*)")
+				print("Time: " ..time)
+				print("Status: "..status)
+		end
+		if str~="" then print(str) end
+		
+		q = uart.read(0,'*l',uart.NO_TIMEOUT)
+		if q == "q" then break end
+	end
 end
