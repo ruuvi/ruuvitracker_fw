8763e86b0f09f0fb0c22540ee76e39b787a3c4bb
SeppoTakalo <seppo.takalo@iki.fi>
2013-01-01 22:20:23 +0200

    Initial test on NMEA  parser

--- romfs/gps.lua
+++ romfs/gps.lua
@@ -1,4 +1,5 @@
 require('gsm')
+require('tracker_api')
 
 gps_enabled = 0
 gps_uart = 1
@@ -10,18 +11,34 @@ function gps_enable()
 	gps_enabled = 1
 end
 
-function gps_monitor()
+function gps_parse_rmc(line)
+			local time, status, latitude, ns_indicator, longitude,
+			ew_indicator, speed = line:match("^%$GPRMC,([^,]*),([^,]*),([^,]*),([^,]*),([^,]*),([^,]*),([^,]*)")
+			if status == "A" then print("Status: Valid location")
+			else print ("Not valid location") return end
+			latitude = latitude/100
+			longitude = longitude/100
+			if ns_indicator == 'S' then latitude = -latitude end
+			if ew_indicator == 'W' then longitude = -longitude end
+			print("Time: " ..time)
+			print("Status: "..status)
+			print("Lat:" ..latitude)
+			print("Lon:" ..longitude)
+			print("Sending event")
+			local event={}
+			event.version="1"
+			event.latitude = string.format("%f",latitude)
+			event.longitude = string.format("%f",longitude)
+			send_event(event)
+end
+
+function gps_looper()
 	if gps_enabled==0 then gps_enable() end
-	
-	
+
+	print("Starting GPS loop. Press 'q' to stop")	
 	while 1 == 1 do
-		str = uart.read(gps_uart,'*l')
-		if str:find("^%$GPRMC") then
-				local time, status, latitude, ns_indicator, longitude,
-			  	ew_indicator, speed = str:match("^%$GPRMC,([^,]*),([^,]*),([^,]*),([^,]*),([^,]*),([^,]*),([^,]*)")
-				print("Time: " ..time)
-				print("Status: "..status)
-		end
+		local str = uart.read(gps_uart,'*l')
+		if str:find("^%$GPRMC") then gps_parse_rmc(str)	end
 		if str~="" then print(str) end
 		
 		q = uart.read(0,'*l',uart.NO_TIMEOUT)
