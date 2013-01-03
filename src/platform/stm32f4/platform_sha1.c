6d0c0af217b929d7afdfdb2ffad3471394812501
SeppoTakalo <seppo.takalo@iki.fi>
2013-01-04 01:18:31 +0200

    Added HMAC_SHA1 to sha1 module

--- src/platform/stm32f4/platform_sha1.c
+++ src/platform/stm32f4/platform_sha1.c
@@ -8,7 +8,9 @@
 #include "sha1.h"
 #include <string.h>
 
-#define SHA1_LEN	40
+#define SHA1_STR_LEN	40
+#define SHA1_LEN	20
+#define BLOCKSIZE	64
 
 static int sha1_gen( lua_State *L )
 {
@@ -22,16 +24,92 @@ static int sha1_gen( lua_State *L )
 	if (!SHA1Result(&sha)) {
         return 0;
     } else {
-    	char s[SHA1_LEN];
+    	char s[SHA1_STR_LEN];
     	int i;
         for(i = 0; i < 5 ; i++)
         {
             sprintf(s+i*8,"%.8x", sha.Message_Digest[i]);
         }
-        lua_pushlstring(L,s,SHA1_LEN);
+        lua_pushlstring(L,s,SHA1_STR_LEN);
         return 1;
     }
 
+}
+
+/* Helper to get hash from library */
+/* TODO: High&Low endianess */
+static void get_hash(SHA1Context *sha, char *p)
+{
+    int i;
+    for(i=0;i<5;i++) {
+        *p++ = (sha->Message_Digest[i]>>24)&0xff;
+        *p++ = (sha->Message_Digest[i]>>16)&0xff;
+        *p++ = (sha->Message_Digest[i]>>8)&0xff;
+        *p++ = (sha->Message_Digest[i]>>0)&0xff;
+    }
+}
+
+#define IPAD 0x36
+#define OPAD 0x5C
+
+static int sha1_hmac( lua_State *L )
+{
+        SHA1Context sha1,sha2;
+        char key[BLOCKSIZE];
+        char hash[SHA1_LEN];
+        int i;
+        size_t len;
+
+        const char *secret,*msg;
+    
+        luaL_checkstring(L,1);
+        msg = luaL_checkstring(L,2);
+    
+        //Get length from LUA (may contain zeroes)
+        secret = lua_tolstring(L,1,&len);
+
+        //Fill with zeroes
+        memset(key, 0, BLOCKSIZE);
+
+        if (len > BLOCKSIZE) {
+                //Too long key, shorten with hash
+                SHA1Reset(&sha1);
+                SHA1Input(&sha1, (const unsigned char *)secret, len);
+                SHA1Result(&sha1);
+                get_hash(&sha1, key);
+        } else {
+                memcpy(key, secret, len);
+        }
+
+        //XOR key with IPAD
+        for (i=0; i<BLOCKSIZE; i++) {
+                key[i] ^= IPAD;
+        }
+
+        //First SHA hash
+        SHA1Reset(&sha1);
+        SHA1Input(&sha1, (const unsigned char *)key, BLOCKSIZE);
+        SHA1Input(&sha1, (const unsigned char *)msg, strlen(msg));
+        SHA1Result(&sha1);
+        get_hash(&sha1, hash);
+
+        //XOR key with OPAD
+        for (i=0; i<BLOCKSIZE; i++) {
+                key[i] ^= IPAD ^ OPAD;
+        }
+
+        //Second hash
+        SHA1Reset(&sha2);
+        SHA1Input(&sha2, (const unsigned char *)key, BLOCKSIZE);
+        SHA1Input(&sha2, (const unsigned char *)hash, SHA1_LEN);
+        SHA1Result(&sha2);
+
+        char s[SHA1_STR_LEN];
+        for(i = 0; i < 5 ; i++) {
+                sprintf(s+i*8,"%.8x", sha2.Message_Digest[i]);
+        }
+        lua_pushlstring(L,s,SHA1_STR_LEN);
+        return 1;
 }
 
 #define MIN_OPT_LEVEL 2
@@ -41,6 +119,7 @@ static int sha1_gen( lua_State *L )
 const LUA_REG_TYPE sha1_map[] =
 { 
   { LSTRKEY("gen") , LFUNCVAL(sha1_gen) },
+  { LSTRKEY("hmac"), LFUNCVAL(sha1_hmac) },
   { LNILKEY, LNILVAL }
 };
 
