ad87c673a9ee7407704b75a06e7f367bfd5ebb8d
SeppoTakalo <seppo.takalo@iki.fi>
2013-01-07 19:52:13 +0200

    Modifed HTTP to use logger and timeouts

--- romfs/http.lua
+++ romfs/http.lua
@@ -1,11 +1,16 @@
 require('common')
 require('gsm')
+require('logging')
 
 module('http', package.seeall)
+
+local logger = Logger:new('http')
+local timeout=10e6 -- 10s
 
 function get(url)
 	local data = ""
 	local user_agent = "RuuviTracker firmware/" .. firmware.version
+	logger:debug("Get request to "..url)
 	--Start HTTP request
 	gsm.cmd([[
 AT+HTTPINIT
@@ -14,24 +19,30 @@ AT+HTTPPARA="URL","]]..url..[["
 AT+HTTPPARA="UA","]]..user_agent..[["
 AT+HTTPPARA="REDIR","1"]])
 	gsm.send('AT+HTTPACTION=0')
-	ret = gsm.wait("^%+HTTPACTION")
+	logger:debug("connecting...")
+	local ret = gsm.wait("^%+HTTPACTION", timeout)
 	local code
 	local len
-	_,_,code,len = string.find(ret,"%+HTTPACTION:0,(%d+),(%d+)")
-	if code == "200" then
-		if gsm.verbose==1 then print("Reading "..len.." bytes from "..url) end
-		gsm.send('AT+HTTPREAD')
-		gsm.wait("^%+HTTPREAD:"..len)
-		--All bytes are data after this
-		data = gsm.read_raw(len)
-		--All bytes read, Wait for OK
-		gsm.wait("^OK")
+	if ret then
+	   logger:debug("connected")
+	   _,_,code,len = string.find(ret,"%+HTTPACTION:0,(%d+),(%d+)")
+	   if code == "200" then
+	      logger:debug("200 OK, Reading "..len.." bytes")
+	      gsm.send('AT+HTTPREAD')
+	      gsm.wait("^%+HTTPREAD:"..len)
+	      --All bytes are data after this
+	      data = gsm.read_raw(len)
+	      --All bytes read, Wait for OK
+	      gsm.wait("^OK")
+	   else
+	      logger:warn("Error "..code)
+	   end
 	else
-		print("Error "..code)
+	   logger:warn("timeout!")
 	end
 	--Terminate HTTP commands
 	gsm.cmd("AT+HTTPTERM")
-	return data
+	return code, data
 end
 
 --URL Encode
@@ -47,40 +58,46 @@ function url_encode(str)
 end
 
 function post(url, data, content)
-	if content == nil then
-		data = url_encode(data)
-		content = 'application/x-www-form-urlencoded'
-	end
-	local user_agent = "RuuviTracker firmware/" .. firmware.version
-	gsm.cmd([[
+   if content == nil then
+      data = url_encode(data)
+      content = 'application/x-www-form-urlencoded'
+   end
+   local user_agent = "RuuviTracker firmware/" .. firmware.version
+   logger:debug("POST request to "..url)
+   gsm.cmd([[
 AT+HTTPINIT
 AT+HTTPPARA="CID","1"
 AT+HTTPPARA="URL","]]..url..[["
 AT+HTTPPARA="UA","]]..user_agent..[["
 AT+HTTPPARA="CONTENT","]]..content..[["
 AT+HTTPPARA="REDIR","1"]])
-	gsm.send('AT+HTTPDATA='..data:len()..',1000')
-	gsm.wait("^DOWNLOAD")
-	gsm.send(data)
-	gsm.wait("^OK")
-	gsm.send('AT+HTTPACTION=1')
-	local ret = gsm.wait("^%+HTTPACTION")
-	local code
-	local len
-	data = ""
-	_,_,code,len = string.find(ret,"%+HTTPACTION:1,(%d+),(%d+)")
-	if code == "200" then
-		if gsm.verbose==1 then print("Reading "..len.." bytes from "..url) end
-		gsm.send('AT+HTTPREAD')
-		gsm.wait("^%+HTTPREAD:"..len)
-		--All bytes are data after this
-		data = gsm.read_raw(len)
-		--All bytes read, Wait for OK
-		gsm.wait("^OK")
-	else
-		print("Error "..code)
-	end
-	gsm.cmd("AT+HTTPTERM")
-	return code,data
+   gsm.send('AT+HTTPDATA='..data:len()..',1000')
+   gsm.wait("^DOWNLOAD")
+   gsm.send(data)
+   gsm.wait("^OK")
+   gsm.send('AT+HTTPACTION=1')
+   logger:debug("connecting")
+   local ret = gsm.wait("^%+HTTPACTION", timeout)
+   local code
+   local len
+   local data = ""
+   if ret then
+      _,_,code,len = string.find(ret,"%+HTTPACTION:1,(%d+),(%d+)")
+      if code == "200" then
+	 logger:debug("200 OK, Reading "..len.." bytes")
+	 gsm.send('AT+HTTPREAD')
+	 gsm.wait("^%+HTTPREAD:"..len)
+	 --All bytes are data after this
+	 data = gsm.read_raw(len)
+	 --All bytes read, Wait for OK
+	 gsm.wait("^OK")
+      else
+	 logger:warn("Error "..code)
+      end
+   else
+      logger:warn("timeout")
+   end
+   gsm.cmd("AT+HTTPTERM")
+   return code,data
 end
 
