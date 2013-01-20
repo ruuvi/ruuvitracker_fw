0db327f2c74602de73996e61643355a69b526750
SeppoTakalo <seppo.takalo@iki.fi>
2013-01-20 22:54:16 +0200

    Enabling Hardware floating points

--- src/platform/stm32f4/conf.py
+++ src/platform/stm32f4/conf.py
@@ -3,8 +3,11 @@ import fnmatch
 import glob
 import os
 
+# Place to keep ST micro's library
 fwlibdir= 'src/platform/%s/FWLib' % platform
 
+# Helper function to download library for us
+# requires wget utility
 def fetch_lib(dir, url):
 	print "Downloading %s " % url, "to %s" % dir
 	path = os.getcwd()
@@ -16,39 +19,39 @@ def fetch_lib(dir, url):
 	f.close()
 	os.chdir(path)
 
-#Download FWlib if not exist
+# Download FWlib if not exist
 try:
 	stat = os.stat(fwlibdir+"/.downloaded")
 except OSError:
-	#fetch_lib(fwlibdir, "http://www.st.com/internet/com/SOFTWARE_RESOURCES/SW_COMPONENT/FIRMWARE/stm32f4_dsp_stdperiph_lib.zip")
 	fetch_lib(fwlibdir, "http://www.st.com/internet/com/SOFTWARE_RESOURCES/SW_COMPONENT/FIRMWARE/stm32_f105-07_f2_f4_usb-host-device_lib.zip")
 
-# STDPeriphdir = fwlibdir+"/STM32F4xx_DSP_StdPeriph_Lib_V1.0.1/Libraries/STM32F4xx_StdPeriph_Driver"
-# CMSISdir = fwlibdir+"/STM32F4xx_DSP_StdPeriph_Lib_V1.0.1/Libraries/CMSIS"
+# Specify different paths inside of ST's library
 STDPeriphdir = fwlibdir+"/STM32_USB-Host-Device_Lib_V2.1.0/Libraries/STM32F4xx_StdPeriph_Driver"
 CMSISdir = fwlibdir+"/STM32_USB-Host-Device_Lib_V2.1.0/Libraries/CMSIS"
 USBdev = fwlibdir+"/STM32_USB-Host-Device_Lib_V2.1.0/Libraries/STM32_USB_Device_Library"
 USBotg = fwlibdir+"/STM32_USB-Host-Device_Lib_V2.1.0/Libraries/STM32_USB_OTG_Driver"
 
+# construct Include path
 comp.Append(CPPPATH = [STDPeriphdir+"/inc" , CMSISdir+"/Include", CMSISdir+"/Device/ST/STM32F4xx/Include/", USBdev+"/Core/inc/", USBotg+"/inc/", USBdev+"/Class/cdc/inc/" ])
 
-STD_files = "stm32f4xx_adc.c stm32f4xx_dac.c stm32f4xx_dma.c stm32f4xx_exti.c stm32f4xx_gpio.c stm32f4xx_i2c.c stm32f4xx_rcc.c stm32f4xx_usart.c stm32f4xx_spi.c stm32f4xx_tim.c misc.c"
+# Files to compile from ST's libraries
+STD_files = "stm32f4xx_adc.c stm32f4xx_dac.c stm32f4xx_dma.c stm32f4xx_exti.c stm32f4xx_gpio.c stm32f4xx_i2c.c stm32f4xx_rcc.c " \
+    + "stm32f4xx_usart.c stm32f4xx_spi.c stm32f4xx_tim.c misc.c"
 USBotg_files = "usb_dcd.c usb_dcd_int.c usb_core.c"
 USBcore_files = "usbd_req.c usbd_core.c usbd_ioreq.c"
 USBclass_files =USBdev+"/Class/cdc/src/usbd_cdc_core.c"
 
+# join lib files with correct paths
 fwlib_files = " ".join( [ STDPeriphdir+"/src/%s" % ( f ) for f in STD_files.split() ] ) \
 	+" "+ CMSISdir+"/Device/ST/STM32F4xx/Source/Templates/gcc_ride7/startup_stm32f4xx.s" \
 	+" "+ " ".join( [ USBdev+"/Core/src/%s" % ( f ) for f in USBcore_files.split() ] ) \
 	+" "+ " ".join( [ USBotg+"/src/%s" % ( f ) for f in USBotg_files.split() ] ) \
 	+" "+ USBclass_files
 
-#print "FWLib: %s " % fwlib_files 
-
+# Files to compile from this folder
 specific_files = "system_stm32f4xx.c platform.c platform_int.c uart.c platform_i2c.c platform_sha1.c sha1.c" \
     +" usb_bsp.c usbd_desc.c usbd_usr.c usbd_cdc_vcp.c stm32_usb.c"
 
-#ldscript = "stm32f4xx_flash.ld" 
 ldscript = "stm32.ld"
   
 # Prepend with path
@@ -56,6 +59,7 @@ specific_files = fwlib_files + " " + " ".join( [ "src/platform/%s/%s" % ( platfo
 specific_files += " src/platform/cortex_utils.s src/platform/arm_cortex_interrupts.c"
 ldscript = "src/platform/%s/%s" % ( platform, ldscript )
 
+# Flags for compiler
 comp.Append(CPPDEFINES = ["FOR" + cnorm( comp[ 'cpu' ] ),"FOR" + cnorm( comp[ 'board' ] ),'gcc'])
 comp.Append(CPPDEFINES = [ 'USE_STDPERIPH_DRIVER', 'STM32F4XX', 'CORTEX_M4', 'HSE_VALUE=12000000', 'USE_EMBEDDED_PHY', 'USE_USB_OTG_FS'])
 
@@ -67,7 +71,8 @@ comp.Append(LINKFLAGS = ['-nostartfiles', '-nostdlib','-T',ldscript,'-Wl,--gc-se
 comp.Append(ASFLAGS = ['-x','assembler-with-cpp','-c','-Wall','$_CPPDEFFLAGS'])
 comp.Append(LIBS = ['c','gcc','m'])
 
-TARGET_FLAGS = ['-mcpu=cortex-m4', '-mthumb']
+#TARGET_FLAGS = ['-mthumb', '-mcpu=cortex-m4' ]  # Software Floating point
+TARGET_FLAGS = ['-mthumb', '-mcpu=cortex-m4', '-mfloat-abi=hard', '-mfpu=fpv4-sp-d16'] # Hardware floating point. Requires supporting toolchain
 
 # Configure General Flags for Target
 comp.Prepend(CCFLAGS = [TARGET_FLAGS,'-mlittle-endian'])
