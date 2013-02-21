227dd3b94fcaacaddda36c291e243865b6ff1ef0
SeppoTakalo <seppo.takalo@iki.fi>
2013-02-21 18:40:56 +0200

    Modified programming script to enable RuuviB1 board

--- ruuvi_program.sh
+++ ruuvi_program.sh
@@ -1,4 +1,18 @@
-./ruuvi_build.sh || exit 1
-#st-flash write elua_lua_stm32f407vg.bin 0x08000000
-dfu-util -i 0 -a 0 -D elua_lua_stm32f415rg.bin -s 0x08000000:leave
+#!/bin/sh
+#
+# Programming script for RuuviTracker
+# Author: Seppo Takalo
+
+source ./ruuvi.conf
+
+if [ "$BOARD" == "RUUVIA" ]; then
+    BINARY="elua_lua_stm32f415rg.bin"
+elif [ "$BOARD" == "RUUVIB1" ]; then
+    BINARY="elua_lua_stm32f407vg.bin"
+else
+    echo "ERROR, Unknown board"
+    exit 1
+fi
+
+dfu-util -i 0 -a 0 -D $BINARY -s 0x08000000:leave
 
