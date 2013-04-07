3b32f725d81ff1f06be77d0a8129d06e8475f98a
Goksel Goktas <goksel@colossal.fi>
2013-04-07 15:16:49 +0300

    hello-birds example added because why the heck not?

---
+++ romfs/hello-birds.lua
@@ -0,0 +1,5 @@
+-- This doesn't really do anything, so don't even try to get something out of
+-- it.  The idea is to see if after all the minifying does everything still
+-- work.
+
+print("no-operation: hello, birds!\n");
