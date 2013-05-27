2719fc4f8d62aeaff9f5c108a130a9b863b1bef5
SeppoTakalo <seppo.takalo@iki.fi>
2013-05-27 23:53:00 +0300

    Modify USB Configuration Descriptor to ask for 500mA.

--- src/platform/stm32f4/stm32_usb.c
+++ src/platform/stm32f4/stm32_usb.c
@@ -12,8 +12,23 @@
    
 __ALIGN_BEGIN USB_OTG_CORE_HANDLE    USB_OTG_dev __ALIGN_END ;
 
+/* See USB Configuration fields from http://www.beyondlogic.org/usbnutshell/usb5.shtml */
+/* Define Attribute positions in Configuration Descriptor */
+#define BMATTRIBUTES 7
+#define BMAXPOWER    8
+/* Attributes */
+#define BMATTR_BUS_POWERED 0x80
+#define BMAXPOWER_500mA    0xFA
+
 void usb_init()
 {
+  uint16_t len;
+  uint8_t *desc;
+
+  /* Modify STM32 Library's pre-definded descriptor for RuuviTracker */
+  desc = USBD_CDC_cb.GetConfigDescriptor(0, &len);
+  desc[BMATTRIBUTES] = BMATTR_BUS_POWERED; /* Define Ruuvi as a Bus powered device */
+  desc[BMAXPOWER] = BMAXPOWER_500mA;       /* Ask for 500mA power. */
 
   USBD_Init(&USB_OTG_dev,
 #ifdef USE_USB_OTG_HS 
