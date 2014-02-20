290adeba72b5cc8a7cdc4c58e330ff7c00baaeba
SeppoTakalo <seppo.takalo@iki.fi>
2014-02-20 23:54:18 +0200

    Basic debug(...) function

---
+++ drivers/debug.h
@@ -0,0 +1,28 @@
+#ifndef _DEBUG_H
+#define _DEBUG_H
+
+#include "ch.h"
+#include "hal.h"
+#include "chprintf.h"
+
+#ifndef NO_DEBUG
+
+/* Assume debug output to be this USB-serial port */
+extern SerialUSBDriver SDU2;
+#define printf(...) chprintf((BaseSequentialStream *)&SDU2, __VA_ARGS__)
+
+#define D_ENTER() printf("%s:%s(): enter\r\n", __FILE__, __FUNCTION__)
+#define D_EXIT() printf("%s:%s(): exit\r\n", __FILE__, __FUNCTION__)
+#define _DEBUG(...) do{ \
+	  printf("%s:%s:%d: ", __FILE__, __FUNCTION__, __LINE__); \
+	  printf(__VA_ARGS__); \
+     } while(0)
+
+#else
+
+#define D_ENTER()
+#define D_EXIT()
+#define _DEBUG(...)
+#endif /* NO_DEBUG */
+
+#endif /* _DEBUG_H */
