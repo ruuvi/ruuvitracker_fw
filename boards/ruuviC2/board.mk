f709277a43ebb3d353d6c9d10fe7a4be5b18744a
Eero af Heurlin <rambo@iki.fi>
2014-02-23 14:45:02 +0200

    merge the lowpower stuff from my devel branch

--- boards/ruuviC2/board.mk
+++ boards/ruuviC2/board.mk
@@ -4,6 +4,7 @@ BOARDSRC = boards/ruuviC2/board.c boards/ruuviC2/power.c
 # Required include directories
 BOARDINC = boards/ruuviC2
 
+ENABLE_WFI_IDLE = yes
 USE_FPU = yes
 
 .DEFAULT_GOAL := all
--- main.c
+++ main.c
@@ -22,6 +22,7 @@
 #include "test.h"
 
 #include "shell.h"
+#include "drivers/debug.h"
 #include "chprintf.h"
 #include "power.h"
 #include "drivers/usb_serial.h"
@@ -168,7 +169,6 @@ static void cmd_http(BaseSequentialStream *chp, int argc, char *argv[])
     }
 }
 
-
 static const ShellCommand commands[] = {
     {"mem", cmd_mem},
     {"threads", cmd_threads},
@@ -176,6 +176,8 @@ static const ShellCommand commands[] = {
     {"gps_test", cmd_gps},
     {"gsm", cmd_gsm},
     {"http", cmd_http},
+    {"stop", cmd_stop},
+    {"standby", cmd_standby},
     {NULL, NULL}
 };
 
@@ -212,6 +214,7 @@ static void Thread1(void *arg)
 /*
  * Application entry point.
  */
+static const EXTConfig extcfg; 
 int main(void)
 {
     Thread *shelltp = NULL;
@@ -232,7 +235,11 @@ int main(void)
     usb_serial_init();
 
     /* Initializes reset button PA0 */
+    /**
+     * This messes my button wakeup
     button_init();
+     */
+    extStart(&EXTD1, &extcfg);
 
     /*
      * Shell manager initialization.
