ca2ce29fd42d25a4f67ddbd926e8e81a51f4763f
Eero af Heurlin <rambo@iki.fi>
2015-03-03 23:49:17 +0200

    we have MMA8652 not MMA7660

--- stmhal/boards/RUUVITRACKER_C3/mpconfigboard.h
+++ stmhal/boards/RUUVITRACKER_C3/mpconfigboard.h
@@ -6,7 +6,7 @@
 
 #define MICROPY_HW_HAS_SWITCH       (1)
 #define MICROPY_HW_HAS_SDCARD       (1)
-#define MICROPY_HW_HAS_MMA7660      (1)
+#define MICROPY_HW_HAS_MMA7660      (0)
 #define MICROPY_HW_HAS_LIS3DSH      (0)
 #define MICROPY_HW_HAS_LCD          (1)
 #define MICROPY_HW_ENABLE_RNG       (1)
