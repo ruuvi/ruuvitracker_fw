2198899f6f051e5fc5d5c1ca90cf19048a29d5ad
Eero af Heurlin <rambo@iki.fi>
2015-03-08 16:06:19 +0200

    Add uasyncio support
    logging is needed by uasyncio core
    collections.deque is needed by queues

---
+++ stmhal/boards/RUUVITRACKER_C3/copy_to_board/collections/README.md
@@ -0,0 +1,6 @@
+# collections (MicroPython)
+
+The originals are from:
+
+  - https://github.com/micropython/micropython-lib/blob/master/collections/collections/__init__.py
+  - https://github.com/micropython/micropython-lib/blob/master/collections.deque/collections/deque.py
---
+++ stmhal/boards/RUUVITRACKER_C3/copy_to_board/logging.py
@@ -0,0 +1,65 @@
+# Dummy file to preclude import errors
+# Should be reimplemented for MicroPython
+# Reason:
+# Basic, useful module, by CPython impl depends on module "string" which
+# uses metaclasses.
+
+CRITICAL = 50
+ERROR    = 40
+WARNING  = 30
+INFO     = 20
+DEBUG    = 10
+NOTSET   = 0
+
+_level_dict = {
+    CRITICAL: "CRIT",
+    ERROR: "ERROR",
+    WARNING: "WARN",
+    INFO: "INFO",
+    DEBUG: "DEBUG",
+}
+
+class Logger:
+
+    def __init__(self, name):
+        self.level = NOTSET
+        self.name = name
+
+    def _level_str(self, level):
+        if level in _level_dict:
+            return _level_dict[level]
+        return "LVL" + str(level)
+
+    def log(self, level, msg, *args):
+        if level >= (self.level or _level):
+            print(("%s:%s:" + msg) % ((self._level_str(level), self.name) + args))
+
+    def debug(self, msg, *args):
+        self.log(DEBUG, msg, *args)
+
+    def info(self, msg, *args):
+        self.log(INFO, msg, *args)
+
+    def warning(self, msg, *args):
+        self.log(WARNING, msg, *args)
+
+    def error(self, msg, *args):
+        self.log(ERROR, msg, *args)
+
+    def critical(self, msg, *args):
+        self.log(CRITICAL, msg, *args)
+
+
+_level = INFO
+_loggers = {}
+
+def getLogger(name):
+    if name in _loggers:
+        return _loggers[name]
+    l = Logger(name)
+    _loggers[name] = l
+    return l
+
+def basicConfig(level=INFO):
+    global _level
+    _level = level
