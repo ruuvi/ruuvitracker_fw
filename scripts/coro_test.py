3e8114698d3dc80ffc8cd8891058bb83f595eacf
Eero af Heurlin <rambo@iki.fi>
2015-03-15 20:57:39 +0200

    heartbeat example but there is a problem they do not blink at the same time...

---
+++ scripts/coro_test.py
@@ -0,0 +1,28 @@
+import pyb
+import rtb
+
+import logging
+#logging.basicConfig(logging.DEBUG)
+from uasyncio.core import get_event_loop, sleep
+
+# This is a coroutine, we do not use the decorator to indicate that due to resource constrainst of pyboard
+def heartbeat(ledno=1):
+    led = pyb.LED(ledno)
+    sleep1 = 2
+    sleep2 = 0.1
+    sleep3 = 0.2
+    while True:
+        yield from sleep(sleep1)
+        led.on()
+        yield from sleep(sleep2)
+        led.off()
+        yield from sleep(sleep3)
+        led.on()
+        yield from sleep(sleep2)
+        led.off()
+
+
+get_event_loop().create_task(heartbeat(1))
+get_event_loop().create_task(heartbeat(2))
+
+get_event_loop().run_forever()
