4289a3c49a577c87db2f9adf18cd9c49914e95df
Eero af Heurlin <rambo@iki.fi>
2015-03-16 20:38:32 +0200

    we do not support the pyboard lcd skin

--- stmhal/boards/RUUVITRACKER_C3/mpconfigboard.h
+++ stmhal/boards/RUUVITRACKER_C3/mpconfigboard.h
@@ -12,7 +12,7 @@
 #define MICROPY_HW_HAS_SDCARD       (0)
 #define MICROPY_HW_HAS_MMA7660      (0)
 #define MICROPY_HW_HAS_LIS3DSH      (0)
-#define MICROPY_HW_HAS_LCD          (1)
+#define MICROPY_HW_HAS_LCD          (0)
 #define MICROPY_HW_ENABLE_RNG       (1)
 #define MICROPY_HW_ENABLE_RTC       (1)
 #define MICROPY_HW_ENABLE_TIMER     (1)
