58a68df70d963ba8a44468575cb61e1be53cfc60
Eero af Heurlin <rambo@iki.fi>
2015-03-23 23:30:11 +0200

    override the eventloop and use millisecond delays

---
+++ stmhal/boards/RUUVITRACKER_C3/copy_to_board/rtb/eventloop.py
@@ -0,0 +1,14 @@
+import pyb
+import uasyncio.core
+
+class RTEventLoop(uasyncio.core.EventLoop):
+    def time(self):
+        return pyb.millis()
+
+    def wait(self, delay):
+        start = pyb.millis()
+        while pyb.elapsed_millis(start) < delay:
+            pyb.delay(10)
+
+uasyncio.core._event_loop = None
+uasyncio.core._event_loop_class = RTEventLoop
