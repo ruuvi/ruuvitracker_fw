140c3d679bab6ff253b1fb5614ade5e2fa9b6154
Eero af Heurlin <rambo@iki.fi>
2015-03-23 23:30:24 +0200

    this now works as expected

--- scripts/coro_test.py
+++ scripts/coro_test.py
@@ -2,15 +2,16 @@ import pyb
 
 import logging
 #logging.basicConfig(logging.DEBUG)
+import rtb.eventloop
 from uasyncio.core import get_event_loop, sleep
 
 # This is a coroutine, we do not use the decorator to indicate that due to resource constrainst of pyboard
 def heartbeat(ledno=1):
     led = pyb.LED(ledno)
-    sleep1 = 2
+    sleep1 = 2000
     # It seems that when RTC is working we cannot really do subsecond sleeps, and if it's not working we cannot do concurrent sleeps...
-    sleep2 = 0.2
-    sleep3 = 0.5
+    sleep2 = 100
+    sleep3 = 250
     while True:
         yield from sleep(sleep1)
         led.on()
@@ -32,7 +33,7 @@ def busylooper():
         yield
 
 get_event_loop().create_task(heartbeat(1))
-#get_event_loop().create_task(busylooper())
+get_event_loop().create_task(busylooper())
 get_event_loop().create_task(heartbeat(2))
 
 get_event_loop().run_forever()
