690f5cd3ea0c2b9a0296ac1acb0b0760475df89d
Eero af heurlin <eero.afheurlin@iki.fi>
2015-04-20 23:04:15 +0300

    adapt the unix port uasyncio epoll eventloop to pyboard (well, partly the actual IO side is untested

--- stmhal/boards/RUUVITRACKER_C3/copy_to_board/rtb/eventloop.py
+++ stmhal/boards/RUUVITRACKER_C3/copy_to_board/rtb/eventloop.py
@@ -1,14 +1,2 @@
-import pyb
-import uasyncio.core
-
-class RTEventLoop(uasyncio.core.EventLoop):
-    def time(self):
-        return pyb.millis()
-
-    def wait(self, delay):
-        start = pyb.millis()
-        while pyb.elapsed_millis(start) < delay:
-            pyb.delay(1)
-
-uasyncio.core._event_loop = None
-uasyncio.core._event_loop_class = RTEventLoop
+import uasyncio.pybselect
+# TODO: get rid of imports to this file
---
+++ stmhal/boards/RUUVITRACKER_C3/copy_to_board/uasyncio/pybselect.py
@@ -0,0 +1,143 @@
+# Adapted from https://github.com/micropython/micropython-lib/blob/master/uasyncio/uasyncio/__init__.py
+import pyb
+import select
+from .core import *
+
+class PybPollEventLoop(EventLoop):
+    """PyBoard select.poll based eventloop, NOTE: uses *milliseconds* as the time unit"""
+    def __init__(self):
+        EventLoop.__init__(self)
+        self.poller = select.poll()
+
+    def add_reader(self, fd, cb, *args):
+        if __debug__:
+            log.debug("add_reader%s", (fd, cb, args))
+        self.poller.register(fd, 1, (cb, args))
+
+    def remove_reader(self, fd):
+        if __debug__:
+            log.debug("remove_reader(%s)", fd)
+        self.poller.unregister(fd)
+
+    def add_writer(self, fd, cb, *args):
+        if __debug__:
+            log.debug("add_writer%s", (fd, cb, args))
+        self.poller.register(fd, 2, (cb, args))
+
+    def remove_writer(self, fd):
+        if __debug__:
+            log.debug("remove_writer(%s)", fd)
+        try:
+            self.poller.unregister(fd)
+# I don't think we have this on pyboard
+#        except OSError as e:
+#            # StreamWriter.awrite() first tries to write to an fd,
+#            # and if that succeeds, yield IOWrite may never be called
+#            # for that fd, and it will never be added to poller. So,
+#            # ignore such error.
+#            if e.args[0] != errno.ENOENT:
+#                raise
+        except Exception as e:
+            raise
+
+    def time(self):
+        return pyb.millis()
+
+    def wait(self, delay):
+        if __debug__:
+            log.debug("epoll.wait(%d)", delay)
+        if delay == -1:
+            res = self.poller.poll(-1)
+        else:
+            res = self.poller.poll(delay)
+        #log.debug("epoll result: %s", res)
+        for cb, ev in res:
+            if __debug__:
+                log.debug("Calling IO callback: %s%s", cb[0], cb[1])
+            cb[0](*cb[1])
+
+# I'm not sure we actually can/want to use these
+class StreamReader:
+
+    def __init__(self, s):
+        self.s = s
+
+    def read(self, n=-1):
+        s = yield IORead(self.s)
+        while True:
+            res = self.s.read(n)
+            if res is not None:
+                break
+            log.warn("Empty read")
+        if not res:
+            yield IOReadDone(self.s)
+        return res
+
+    def readline(self):
+        if __debug__:
+            log.debug("StreamReader.readline()")
+        s = yield IORead(self.s)
+        if __debug__:
+            log.debug("StreamReader.readline(): after IORead: %s", s)
+        while True:
+            res = self.s.readline()
+            if res is not None:
+                break
+            log.warn("Empty read")
+        if not res:
+            yield IOReadDone(self.s)
+        if __debug__:
+            log.debug("StreamReader.readline(): res: %s", res)
+        return res
+
+    def aclose(self):
+        yield IOReadDone(self.s)
+        self.s.close()
+
+    def __repr__(self):
+        return "<StreamReader %r>" % self.s
+
+
+class StreamWriter:
+
+    def __init__(self, s):
+        self.s = s
+
+    def awrite(self, buf):
+        # This method is called awrite (async write) to not proliferate
+        # incompatibility with original asyncio. Unlike original asyncio
+        # whose .write() method is both not a coroutine and guaranteed
+        # to return immediately (which means it has to buffer all the
+        # data), this method is a coroutine.
+        sz = len(buf)
+        if __debug__:
+            log.debug("StreamWriter.awrite(): spooling %d bytes", sz)
+        while True:
+            res = self.s.write(buf)
+            # If we spooled everything, return immediately
+            if res == sz:
+                if __debug__:
+                    log.debug("StreamWriter.awrite(): completed spooling %d bytes", res)
+                return
+            if res is None:
+                res = 0
+            if __debug__:
+                log.debug("StreamWriter.awrite(): spooled partial %d bytes", res)
+            assert res < sz
+            buf = buf[res:]
+            sz -= res
+            s2 = yield IOWrite(self.s)
+            #assert s2.fileno() == self.s.fileno()
+            if __debug__:
+                log.debug("StreamWriter.awrite(): can write more")
+
+    def aclose(self):
+        yield IOWriteDone(self.s)
+        self.s.close()
+
+    def __repr__(self):
+        return "<StreamWriter %r>" % self.s
+
+# This mucks the eventloop 
+import uasyncio.core
+uasyncio.core._event_loop_class = PybPollEventLoop
